4bec4c400fb4ed033589fc5d264a6299a4a84c9c
Ana Paula Chequer <anapaularib@hotmail.com>
2022-10-09 09:33:52 -0300

    Primeiro Commit

---
+++ operacoes-crud/src/main/java/br/com/ana/operacoescrud/repository/ContatoRepository.java
@@ -0,0 +1,17 @@
+package br.com.ana.operacoescrud.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.ana.operacoescrud.domain.Contato;
+
+@Repository
+public interface ContatoRepository extends JpaRepository<Contato, Long> {
+	
+}
+	
+	
+	
---
+++ operacoes-crud/src/test/java/br/com/ana/operacoescrud/OperacoesCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ana.operacoescrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OperacoesCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
